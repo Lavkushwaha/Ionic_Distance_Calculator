a580d26f4d2f2a79ffd92c6106825f19cbc81eb3
unknown <kush373@gmail.com>
2019-06-19 02:38:16 -0700

    Calculate distance service created and geolocation will be implanted on it

--- src/app/service/calculate.service.ts
+++ src/app/service/calculate.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CalculateService {
 
-  constructor() { }
-}
+//https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=mandla madhya pradesh&destinations=jabalpur&key=AIzaSyCqOnGO1dxEHnqFr5ox6CezEntcZfPEpNE
+
+
+export class CalculateService {
+  url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric';
+  key = 'AIzaSyCqOnGO1dxEHnqFr5ox6CezEntcZfPEpNE';
+
+  constructor(private http: HttpClient, private geolocation: Geolocation) { }
+
+
+  
+
+  calDistance(origin:string,destination:string){
+  
+
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      console.log('lattitude and logintudes are :',resp.coords.latitude,resp.coords.longitude);
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  
+
+    return this.http.get(`${this.url}&origins=${origin}&destinations=${destination}&key=${this.key}`);
+  }
+
+  
+  getCurrentLoc(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      console.log('lattitude and logintudes are :',resp.coords.latitude,resp.coords.longitude);
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+
+  
+   
+  
+
+  
+  }
