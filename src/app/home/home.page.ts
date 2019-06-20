620f8b29497b6ad1469d19f63e5653545b6c168c
unknown <kush373@gmail.com>
2019-06-20 11:06:22 -0700

    final project for evaluation and update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,20 +10,49 @@ import { CalculateService } from '../service/calculate.service';
 })
 export class HomePage {
 
+  location_distance ;
+  destination_address ;
+  origin_address ;
+  travel_duration ;
+  location ;
+  
   constructor(private calculateService : CalculateService) {
     
 
   }
-
+  
+ 
 
   ngOnint(){
+    this.location_distance=null;
+    this.destination_address=null;
+    this.origin_address=null;
+    this.travel_duration=null;
+    this.location=null;
     
   }
 
-  async changeDistance(location){
-    var results = await this.calculateService.calDistance('jabalpur',location)
-    results.subscribe(res=>{
-      console.log(res['rows']['0']['elements']['0']['distance']['text'])
+  reset(){
+    this.location_distance=null;
+    this.destination_address=null;
+    this.origin_address=null;
+    this.travel_duration=null;
+    this.location=null;
+  }
+
+  async changeDistance(location)
+  {
+
+
+
+      var results = await this.calculateService.calDistance('jabalpur',location)
+      results.subscribe(res=>{
+      console.log(res)
+      this.location_distance = res['rows']['0']['elements']['0']['distance']['text'];
+      this.destination_address = res['destination_addresses'];
+      this.origin_address = res['origin_addresses'];
+      this.travel_duration = res['rows']['0']['elements']['0']['duration']['text'];
+    
     }); 
   }
   
